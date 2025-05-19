4894fb162d86a0374f78175d13503f77dbb4ce30
Varun Rao <varunhs0311@gmail.com>
2025-05-19 11:06:56 +0530

    Update BestSeller.jsx

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,7 +7,7 @@ const BestSeller = () => {
   return (
     <div className="mt-6">
       <p className="text-2xl md:text-3xl font-medium">Best Seller</p>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md: grid-cols-4 gap-3 md: gap-6 lg:grid-cols-5 mt-6">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
         {products
           .filter((product) => product.inStock)
           .slice(0, 5)
