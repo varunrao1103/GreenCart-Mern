3251fd837d796156af5baf4e1fa7c4b390a3e692
Varun Rao <varunhs0311@gmail.com>
2025-05-19 11:07:15 +0530

    Update ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
           );
           scrollTo(0, 0);
         }}
-        className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full"
+        className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white w-full"
       >
         <div className="group cursor-pointer flex items-center justify-center px-2">
           <img
